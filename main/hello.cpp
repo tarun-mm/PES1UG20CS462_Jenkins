bb8ad82b2b8fa13f167f56c930d6a97ba86a8b1e
Tarun M <tarunsetty2002@gmail.com>
2023-02-19 20:42:50 +0530

    SRN to Name

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello World this is PES1UG20CS462\n";
+  cout << "Hello World this is Tarun M\n";
   cout << "Hello Jenkins\n";
   cout << "I have successfully built and run\n";
   return 0;
